76e699aa9c3cd8e31baeb3f2f51fe1fbb74d1623
Harshit Aditya <harshitaditya27@gmail.com>
2024-02-04 20:49:53 +0530

    Added user registration functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -10,9 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "hello from express endpoint!" });
-});
+// app.get("/api/test", async (req: Request, res: Response) => {
+//   res.json({ message: "hello from express endpoint!" });
+// });
+
+app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
   console.log("Server running on localhost:7000");
