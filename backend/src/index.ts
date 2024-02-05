97e77f1cff999711b366684c244ec52383d48d42
Harshit Aditya <harshitaditya27@gmail.com>
2024-02-05 17:30:42 +0530

    Added user login functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -15,6 +16,7 @@ app.use(cors());
 //   res.json({ message: "hello from express endpoint!" });
 // });
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
