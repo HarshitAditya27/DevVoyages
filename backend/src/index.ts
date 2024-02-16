9d7392f41a599a0e54d688fcf0601fc933e3a5b9
Harshit Aditya <harshitaditya27@gmail.com>
2024-02-16 23:45:55 +0530

    Added cloudinary setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,13 @@ import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
