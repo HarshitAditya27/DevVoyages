fde200e6130cfede229868e09476911807f48ce3
Harshit Aditya <harshitaditya27@gmail.com>
2024-02-16 21:09:10 +0530

    fixed button colors

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -114,7 +114,7 @@ const Register = () => {
       <span>
         <button
           type="submit"
-          className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
+          className="bg-purple-600 text-white p-2 font-bold hover:bg-purple-500 text-xl"
         >
           Create Account
         </button>
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -77,7 +77,7 @@ const SignIn = () => {
         </span>
         <button
           type="submit"
-          className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
+          className="bg-purple-600 text-white p-2 font-bold hover:bg-purple-500 text-xl"
         >
           Login
         </button>
