b06aade5b407614718ef3cd9ddd220f4f4d3cb01
Harshit Aditya <harshitaditya27@gmail.com>
2024-02-24 19:51:04 +0530

    Added search result component

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -3,6 +3,7 @@ import { useSearchContext } from "../contexts/SearchContext";
 import { searchHotels } from "../api-client";
 import * as apiClient from "../api-client";
 import { useState } from "react";
+import SearchResultsCard from "../components/SearchResultsCard";
 
 const Search = () => {
   const search = useSearchContext();
@@ -21,7 +22,28 @@ const Search = () => {
   );
 
   console.log(search);
-  return <>Search Page</>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div className="rounded-lg border vorder-slate-300 p-5 h-fit sticky top-10">
+        <div className="space-y-5">
+          <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
+            Filter By:
+          </h3>
+        </div>
+      </div>
+      <div className="flex flex-col gap-5">
+        <div className="flex justify-between items-center">
+          <span className="text-xl font-bold">
+            {hotelData?.pagination.total} Hotels Found
+            {search.destination ? `in ${search.destination}` : ""}
+          </span>
+        </div>
+        {hotelData?.data.map((hotel) => (
+          <SearchResultsCard hotel={hotel} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
