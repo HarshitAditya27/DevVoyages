44757e4e96873f5165ac0da56b38cb5f45d299ca
Harshit Aditya <harshitaditya27@gmail.com>
2024-02-24 17:16:33 +0530

    Added fetch functionality for search hotels

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,7 +1,25 @@
+import { useQuery } from "react-query";
 import { useSearchContext } from "../contexts/SearchContext";
+import { searchHotels } from "../api-client";
+import * as apiClient from "../api-client";
+import { useState } from "react";
 
 const Search = () => {
   const search = useSearchContext();
+  const [page, setPage] = useState<number>(1);
+  const searchParams = {
+    destination: search.destination,
+    checkIn: search.checkIn.toISOString(),
+    checkOut: search.checkOut.toISOString(),
+    adultCount: search.adultCount.toString(),
+    childCount: search.childCount.toString(),
+    page: page.toString(),
+  };
+
+  const { data: hotelData } = useQuery(["searchHotels", searchParams], () =>
+    apiClient.searchHotels(searchParams)
+  );
+
   console.log(search);
   return <>Search Page</>;
 };
