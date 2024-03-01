172dffcf996367887796daaf3f5f7651743fd9b8
Harshit Aditya <harshitaditya27@gmail.com>
2024-03-01 11:16:44 +0530

    Added home page endpoint and component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,6 +15,7 @@ import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
 import MyBookings from "./pages/MyBookings";
+import Home from "./pages/Home";
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -25,7 +26,7 @@ function App() {
           path="/"
           element={
             <Layout>
-              <p>Home Page</p>
+              <Home />
             </Layout>
           }
         />
